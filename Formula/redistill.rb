db273b29183af0381aec02c838a47fbf78d94029
Shahid Shaikh <rwtc66@gmail.com>
2026-01-03 23:44:58 +0530

    Update Redistill to version 1.2.1

--- Formula/redistill.rb
+++ Formula/redistill.rb
@@ -1,27 +1,27 @@
 class Redistill < Formula
   desc "High-performance Redis-compatible key-value store that outperforms Redis"
   homepage "https://github.com/redistill-io/redistill"
-  url "https://github.com/redistill-io/redistill/releases/download/v1.2.0/redistill-1.2.0-x86_64-apple-darwin.tar.gz"
-  sha256 "0d20aabb2c47ee5c8b3dd48f27f66cb2b79418c593ca2ac8f4feecbe23014911"
-  version "1.2.0"
+  url "https://github.com/redistill-io/redistill/releases/download/v1.2.1/redistill-1.2.1-x86_64-apple-darwin.tar.gz"
+  sha256 "5d7a50324784cda162a4d2913ce4be1f9e09c77a826042f02d26669c5e8f914d"
+  version "1.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/redistill-io/redistill/releases/download/v1.2.0/redistill-1.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "0d20aabb2c47ee5c8b3dd48f27f66cb2b79418c593ca2ac8f4feecbe23014911"
+      url "https://github.com/redistill-io/redistill/releases/download/v1.2.1/redistill-1.2.1-x86_64-apple-darwin.tar.gz"
+      sha256 "5d7a50324784cda162a4d2913ce4be1f9e09c77a826042f02d26669c5e8f914d"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/redistill-io/redistill/releases/download/v1.2.0/redistill-1.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "4487bf6be0dc4bf5dcc23ee265edfcb7d5fa3c1a11e6e46f0fb3a1bd21f0598e"
+      url "https://github.com/redistill-io/redistill/releases/download/v1.2.1/redistill-1.2.1-aarch64-apple-darwin.tar.gz"
+      sha256 "97afb24e88f9900212086b2764661be7c62df861e2c8545c9ce4d43b0c4adbbe"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/redistill-io/redistill/releases/download/v1.2.0/redistill-1.2.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "d483fd153f9f8272df68d1bd9d0b3050dd79e271d46f570ae03bb997b93ae945"
+      url "https://github.com/redistill-io/redistill/releases/download/v1.2.1/redistill-1.2.1-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "933f08d2afb46386a39bd5f6503f094c746b5384d4d19a78445fb4904d61758d"
     end
   end
 
